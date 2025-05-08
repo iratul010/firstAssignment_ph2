a44df710f5d37d7e2dc86c97d9c65ccc78b4abec
Ratul Islam <iratul010@gmail.com>
2025-05-08 20:35:38 +0600

    second-task

--- module4/src/index.ts
+++ module4/src/index.ts
@@ -1,3 +1,8 @@
+interface Item {
+  title: string;
+  rating: number;
+}
+
 class FirstAssignmentTask {
   constructor() {}
 
@@ -8,12 +13,24 @@ class FirstAssignmentTask {
       ? case1String.toUpperCase()
       : case1String.toLowerCase();
   }
+  filterByRating(items: Item[]): Item[] {
+    const result = items.filter((item) => item.rating < 5 && item.rating >= 4);
 
-   
+    return result;
+  }
 }
 //1st problem solution---->
 const taskOne = new FirstAssignmentTask();
 console.log(taskOne.textHelper("Hellow", true));
 console.log(taskOne.textHelper("Hellow", false));
-console.log(taskOne.textHelper("Hellow")); 
+console.log(taskOne.textHelper("Hellow"));
 //2nd problem solution---->
+const books: Item[] = [
+  { title: "Book A", rating: 4.3 },
+  { title: "Book A", rating: 3.5 },
+  { title: "Book A", rating: 2.5 },
+  { title: "Book A", rating: 4.5 },
+];
+const taskTwoFilterRating = new FirstAssignmentTask();
+const filterRating = taskTwoFilterRating.filterByRating(books);
+console.log(filterRating);
